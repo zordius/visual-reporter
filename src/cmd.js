0a56b9befdae0afceb94a0ac62c42378533fa2b3
Zordius Chen <1201409+zordius@users.noreply.github.com>
2019-07-31 15:28:10 +0800

    refine args

--- src/cmd.js
+++ src/cmd.js
@@ -1,18 +1,25 @@
-import visualReporter from './'
+#!/usr/bin/env node
+import VisualReporter from './'
 import commander from 'commander'
 
 commander
   .version(require('../package.json').version)
-  .option('-b --baseline', 'baseline directory')
-  .option('-c --current', 'current directory')
-  .option('-r --report', 'report directory')
+  .option('-b --baseline <dir>', 'baseline directory, default is .')
+  .option('-c --compare <dir>', 'compare directory, default is ./compare')
+  .option('-r --report <dir>', 'report directory, default is ./report')
 
 commander
   .command('generate')
   .alias('gen')
   .description('Generate visual report')
+  .action((opts) => {
+    const VR = new VisualReporter({
+      baseline: commander.baseline || process.cwd(),
+      compare: commander.compare || process.cwd() + '/compare',
+      report: commander.report || process.cwd() + '/report',
+    })
+    VR.generateReport()
+  })
 
 commander
   .parse(process.argv)
-
-console.log(commander)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import pixelmatch from 'pixelmatch'
 import fs from 'fs'
+import path from 'path'
 import { PNG } from 'pngjs'
 
 class visualReporter {
@@ -8,6 +9,16 @@ class visualReporter {
 
   constructor (cfg) {
     this.cfg = {...this.defaultCfg, ...cfg}
+  }
+
+  files (dir) {
+    return fs.readdirSync(dir).filter(F => F.match(/\.png$/)).map(F => path.join(dir, F))
+  }
+
+  generateReport () {
+    this.baselineFiles = this.files(this.cfg.baseline)
+    this.compareFiles = this.files(this.cfg.compare)
+    console.log(this.compareFiles)
   }
 }
 
