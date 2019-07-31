8f2c621581be003358e3f9055c63b0035c928945
Zordius Chen <1201409+zordius@users.noreply.github.com>
2019-07-31 16:02:37 +0800

    show add/remove/baseline counts

--- src/index.js
+++ src/index.js
@@ -9,16 +9,32 @@ class visualReporter {
 
   constructor (cfg) {
     this.cfg = {...this.defaultCfg, ...cfg}
+    this.files = {}
   }
 
-  files (dir) {
-    return fs.readdirSync(dir).filter(F => F.match(/\.png$/)).map(F => path.join(dir, F))
+  getFiles (dir) {
+    return fs.readdirSync(dir).filter(F => F.match(/\.png$/))
+  }
+
+  getAdded (list1, list2) {
+    return list2.filter(F => !list1.includes(F))
+  }
+
+  printInfo () {
+    console.log(`
+baseline files: ${this.files.baseline.length}
+compare files : ${this.files.compare.length}
+added files   : ${this.files.add.length}
+removed files : ${this.files.remove.length}
+`)
   }
 
   generateReport () {
-    this.baselineFiles = this.files(this.cfg.baseline)
-    this.compareFiles = this.files(this.cfg.compare)
-    console.log(this.compareFiles)
+    this.files.baseline = this.getFiles(this.cfg.baseline)
+    this.files.compare = this.getFiles(this.cfg.compare)
+    this.files.add = this.getAdded(this.files.baseline, this.files.compare)
+    this.files.remove = this.getAdded(this.files.compare, this.files.baseline)
+    this.printInfo()
   }
 }
 
